1f3fe59e4aa6d4beedf42c7a3ae88b8e9d47c40b
Bogdante <bogdantegiero@gmail.com>
2025-08-06 01:07:28 +0300

    added short link creation

--- src/main/java/com/example/urlshortener/url/services/ShortUrlService.java
+++ src/main/java/com/example/urlshortener/url/services/ShortUrlService.java
@@ -1,5 +1,6 @@
 package com.example.urlshortener.url.services;
 
+import com.example.urlshortener.common.AppConfig;
 import com.example.urlshortener.url.entities.ShortUrl;
 import com.example.urlshortener.url.repositories.ShortUrlRepository;
 import io.seruco.encoding.base62.Base62;
@@ -16,22 +17,29 @@ public class ShortUrlService {
     private final static long CLICKS_LIMIT = 1000;
     private final static int VALID_DAYS = 1;
 
+    private final AppConfig appConfig;
     private final ShortUrlRepository shortUrlRepository;
     private final ActiveShortUrlService activeShortUrlService;
     private final Base62 base62Encoder;
 
-    public ShortUrlService(ShortUrlRepository shortUrlRepository,  ActiveShortUrlService activeShortUrlService) {
+    public ShortUrlService(
+            ShortUrlRepository shortUrlRepository,
+            ActiveShortUrlService activeShortUrlService,
+            AppConfig appConfig
+    ) {
         this.shortUrlRepository = shortUrlRepository;
         this.activeShortUrlService =  activeShortUrlService;
+        this.appConfig = appConfig;
         this.base62Encoder = Base62.createInstance();
     }
+
 
     @Transactional
     public ShortUrl createDefault(String fullUrl) {
         ShortUrl urlEntity = new ShortUrl();
         urlEntity.setFullUrl(fullUrl);
         urlEntity.setValidUntil(getValidUntil(LocalDate.now()));
-        urlEntity =  this.shortUrlRepository.save(urlEntity);
+        urlEntity = this.shortUrlRepository.save(urlEntity);
 
         urlEntity.setShortUrlPathVariable(encodeIdBase62(urlEntity.getId()));
         this.activeShortUrlService.createForShortUrl(urlEntity);
@@ -41,6 +49,10 @@ public class ShortUrlService {
 
     public long getMaxClickCount() {
         return CLICKS_LIMIT;
+    }
+
+    public String generateShortUrlWithDomain(ShortUrl shortUrl) {
+        return this.appConfig.getBaseUrl() + "/" + shortUrl.getShortUrlPathVariable();
     }
 
     private LocalDateTime getValidUntil(LocalDate date) {
